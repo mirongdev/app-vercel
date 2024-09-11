80c6ce807da228baa88bccfdc1304533fdd47e0a
mirongdev <mirongdev@gmail.com>
2024-09-11 22:23:26 +0700

    uji pertama

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Selamat kode Node.js berhasil dijalankan di server Vercel!');
+  res.send('Selamat kode Node.js berhasil dijalankan di server Vercel!, perubahan menunggu proses build selesai yah');
 });
-
+app.get('/cek', (req, res) => {
+    res.send('perubahan menunggu proses build selesai yah');
+  });
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
